b07518509f81df2c5853c1dcd6472d47eedea793
riponahmed <riponahmedjrtr@gmail.com>
2020-05-31 13:15:08 +0600

    Updates

---
+++ precache-manifest.00bfe2239c976d4263a035fb6f590e09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "673871dfa422da5c191ea98dda6aeeaa",
+    "url": "/city-tour/index.html"
+  },
+  {
+    "revision": "792f89eb180dac3f7b6d",
+    "url": "/city-tour/static/css/main.6c02e329.chunk.css"
+  },
+  {
+    "revision": "78c64ebdceddadeeabac",
+    "url": "/city-tour/static/js/2.82abf2a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/city-tour/static/js/2.82abf2a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "792f89eb180dac3f7b6d",
+    "url": "/city-tour/static/js/main.de2e9550.chunk.js"
+  },
+  {
+    "revision": "f1f44dbe709cb75b03b6",
+    "url": "/city-tour/static/js/runtime-main.823f3733.js"
+  }
+]);
